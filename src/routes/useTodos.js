16e5252afdb1100cb9add73c396b16f4479dccfb
haroldZS <haroldzs1024@gmail.com>
2024-01-19 17:59:40 -0400

    feat: Implemented new localStorage item version

--- src/routes/useTodos.js
+++ src/routes/useTodos.js
@@ -8,7 +8,7 @@ function useTodos() {
     sincronizeItem: sincronizeTodos,
     loading,
     error,
-  } = useLocalStorage("TODOS_V1", []);
+  } = useLocalStorage("TODOS_V2", []);
   const [searchValue, setSearchValue] = React.useState("");
   const [openModal, setOpenModal] = React.useState(false);
 
@@ -29,24 +29,26 @@ function useTodos() {
   }
 
   const addTodo = (text) => {
+    const id = newTodoId();
     const newTodos = [...todos];
     newTodos.push({
       conpleted: false,
       text,
+      id,
     });
     saveTodos(newTodos);
   };
 
-  const completeTodo = (text) => {
-    const todoIndex = todos.findIndex((todo) => todo.text === text);
+  const completeTodo = (id) => {
+    const todoIndex = todos.findIndex((todo) => todo.id === id);
 
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
     saveTodos(newTodos);
   };
 
-  const deleteTodo = (text) => {
-    const todoIndex = todos.findIndex((todo) => todo.text === text);
+  const deleteTodo = (id) => {
+    const todoIndex = todos.findIndex((todo) => todo.id === id);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
     saveTodos(newTodos);
@@ -65,8 +67,12 @@ function useTodos() {
     deleteTodo,
     openModal,
     setOpenModal,
-    sincronizeTodos
+    sincronizeTodos,
   };
+}
+
+function newTodoId() {
+  return Date.now().toString(16);
 }
 
 export { useTodos };
